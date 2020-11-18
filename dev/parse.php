d9d973f8135f6c983a247cbd46dee3f38c2cca52
James Blanksby <james@thisismyengine.com>
2020-11-18 10:18:44 +0000

    Updated to prevent parsed tracks from being re-parsed

--- dev/parse.php
+++ dev/parse.php
@@ -8,6 +8,8 @@ foreach (glob(__DIR__ . '/../lib/track/*.gpx') as $file) {
     $file_gpx = __DIR__ . '/../lib/track/' . $name . '.gpx';
     $file_json = __DIR__ . '/../tmp/track/' . $name . '.json';
 
+    if (file_exists($file_json)) continue;
+
     $track = track_parse($file_gpx);
     file_put_contents($file_json, json_encode($track, JSON_PARTIAL_OUTPUT_ON_ERROR));
 }
